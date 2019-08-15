78ade497553ef8f9f02f27508ff8fe1d527e46c2
Darryl Kilzer <darrylkilzer@gmail.com>
2019-08-15 10:29:53 -0600

    Zachary Pfeifer Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// let cornWater = 10 - 10
+// let celereezWater = 10 - 10 
+
+let justCorn = {
+    name: "Just Corn",
+    worth: 100,
+    waterCount: 0,
+}
+
+let celereez = {
+    name: "Celereez",
+    worth: 5,
+    waterCount: 0,
+}
+
